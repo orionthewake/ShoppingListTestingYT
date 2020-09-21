bae9277dba992b538b6e35e63889f60f06d83169
Joe Howard <joe@razeware.com>
2020-09-21 15:14:44 -0400

    androidTest

---
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/ResourceComparerTest.kt
@@ -0,0 +1,35 @@
+package com.androiddevs.shoppinglisttestingyt
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class ResourceComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        val result = resourceComparer.isEqual(context, R.string.app_name, "ShoppingListTestingYT")
+
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentFromGivenString_returnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Hello")
+
+        assertThat(result).isFalse()
+    }
+}
